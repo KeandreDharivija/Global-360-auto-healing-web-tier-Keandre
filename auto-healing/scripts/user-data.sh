5c09472e55f4109fa950621e095b46b6e49e73b1
keandredharivija <kdha0005@student.monash.edu>
2026-08-18 17:52:26 +1000

    feat: add Dockerized NGINX web tier
    - Docker file provided for the NGINX web image
    - Push the image to a Docker registry
    - Update user-data to pull and run the image automatically on each VM
    - include a default index.html page

--- auto-healing/scripts/user-data.sh
+++ auto-healing/scripts/user-data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-dnf install -y nginx
-systemctl enable --now nginx
+dnf install -y docker
+systemctl enable --now docker
+
+docker pull keandre2045/auto-healing-web:latest
+
+docker run -d \
+  --name nginx-web \
+  --restart unless-stopped \
+  -p 80:80 \
+  keandre2045/auto-healing-web:latest
